83df061851668d4bf6df7e5c2f155a536242f767
sixie <si.xie@cern.ch>
2022-12-01 16:20:16 +0100

    corrections for names

--- python/MuonSystemRawToReco_cff.py
+++ python/MuonSystemRawToReco_cff.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 import EventFilter.CSCRawToDigi.cscUnpacker_cfi
 muonCSCDigis = EventFilter.CSCRawToDigi.cscUnpacker_cfi.muonCSCDigis.clone()
-muonCSCDigis.InputObjects = 'rawDataCollector'
+muonCSCDigis.InputObjects = 'rawDataRepacker'
 
 import EventFilter.DTRawToDigi.dtunpacker_cfi
 muonDTDigis = EventFilter.DTRawToDigi.dtunpacker_cfi.muonDTDigis.clone()
-muonDTDigis.inputLabel = 'rawDataCollector'
+muonDTDigis.inputLabel = 'rawDataRepacker'
 
 from RecoLocalMuon.CSCRecHitD.cscRecHitD_cfi import *
 from RecoLocalMuon.DTRecHit.dt1DRecHits_LinearDriftFromDB_cfi import *
